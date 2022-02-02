a2ff868656a11fb4db362bea04c7f79d82b0ea90
Krishna Rajendram Bashyam <krishnarb3@gmail.com>
2022-02-02 15:37:54 -0700

    Add changes to push message to discord

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use clap::Parser;
 use graphql_client::{reqwest::post_graphql_blocking as post_graphql, GraphQLQuery};
 use reqwest::blocking::Client;
 use reqwest::header;
+use serde_json::Value;
+use std::collections::HashMap;
 use std::env;
 
 const USER_AGENT: &str = "graphql-rust/0.10.0";
 const COOKIE_ENV_KEY: &str = "LEETCODE_SESSION";
+const LEETCODE_PROBLEMS_PREFIX: &str = "https://leetcode.com/problems/";
+const DISCORD_WEBHOOK_URL_KEY: &str = "DISCORD_WEBHOOK_URL_KEY";
 
 fn main() -> Result<(), anyhow::Error> {
     let args: Args = Args::parse();
@@ -19,20 +23,23 @@ fn main() -> Result<(), anyhow::Error> {
 
     let cookie = match env::var(COOKIE_ENV_KEY) {
         Ok(cookie_value) => format!("LEETCODE_SESSION={:#?}", cookie_value),
-        Err(_) => panic!("{:#?} env variable not found", COOKIE_ENV_KEY),
+        Err(_) => panic!("{} env variable not found", COOKIE_ENV_KEY),
     };
     let mut headers = header::HeaderMap::new();
     headers.insert("Cookie", header::HeaderValue::from_str(&*cookie).unwrap());
 
-    let client = Client::builder()
+    let leetcode_client = Client::builder()
         .user_agent(USER_AGENT)
         .default_headers(headers)
         .build()?;
 
-    let response_body =
-        post_graphql::<getCompanyTag, _>(&client, "https://leetcode.com/graphql", variables);
+    let response_body = post_graphql::<getCompanyTag, _>(
+        &leetcode_client,
+        "https://leetcode.com/graphql",
+        variables,
+    );
     if response_body.is_err() {
-        panic!("{:#?}", response_body.unwrap_err());
+        panic!("{}", response_body.unwrap_err());
     }
     let response_data: get_company_tag::ResponseData =
         response_body.unwrap().data.expect("Missing response data");
@@ -44,8 +51,24 @@ fn main() -> Result<(), anyhow::Error> {
             let title_slug = question_fields.title_slug;
             result.push(title_slug.to_string());
         }
-        println!("{:#?}", result);
     }
+
+    let random_problem_url = LEETCODE_PROBLEMS_PREFIX.to_string() + &result[0];
+
+    let discord_webhook_url = env::var(DISCORD_WEBHOOK_URL_KEY);
+    if discord_webhook_url.is_ok() {
+        let discord_client = reqwest::blocking::Client::new();
+        let body: Value =
+            serde_json::from_str(&*format!("{{\"content\":\"{}\"}}", random_problem_url))?;
+        let response = discord_client
+            .post(discord_webhook_url.unwrap())
+            .json(&body)
+            .send()?;
+    } else {
+        println!("Discord webhook url is not configured");
+        println!("{}", random_problem_url);
+    }
+
     Ok(())
 }
 
