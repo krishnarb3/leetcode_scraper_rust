682e71e1df7072a1c7c5b69a9dc898608731501e
Krishna Rajendram Bashyam <krishnarb3@gmail.com>
2022-02-02 20:38:34 -0700

    Add aws lambda integration

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,74 @@
 use clap::Parser;
+#[macro_use]
+extern crate lambda_runtime as lambda;
 use graphql_client::{reqwest::post_graphql_blocking as post_graphql, GraphQLQuery};
-use reqwest::blocking::Client;
-use reqwest::header;
+use lambda::handler_fn;
+use log::LevelFilter;
+use rand::Rng;
+use reqwest::{blocking::Client, header};
 use serde_json::Value;
-use std::collections::HashMap;
+use simple_logger::SimpleLogger;
 use std::env;
+use tokio::task;
 
-const USER_AGENT: &str = "graphql-rust/0.10.0";
 const COOKIE_ENV_KEY: &str = "LEETCODE_SESSION";
-const LEETCODE_PROBLEMS_PREFIX: &str = "https://leetcode.com/problems/";
 const DISCORD_WEBHOOK_URL_KEY: &str = "DISCORD_WEBHOOK_URL_KEY";
+const LEETCODE_PROBLEMS_PREFIX: &str = "https://leetcode.com/problems/";
+const RUN_MODE: &str = "RUN_MODE";
+const AWS_LAMBDA_MODE: &str = "AWS_LAMBDA";
+const USER_AGENT: &str = "graphql-rust/0.10.0";
 
-fn main() -> Result<(), anyhow::Error> {
+#[tokio::main]
+async fn main() {
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+        .with_utc_timestamps()
+        .init()
+        .unwrap();
+    let mode = env::var(RUN_MODE);
+    if mode.is_ok() && mode.unwrap() == AWS_LAMBDA_MODE {
+        let func = handler_fn(lambda_fetch);
+        lambda_runtime::run(func).await;
+    } else {
+        task::spawn_blocking(move || {
+            cli_fetch();
+        })
+        .await;
+    }
+}
+
+async fn lambda_fetch(req: Args, context: lambda_runtime::Context) -> Result<(), anyhow::Error> {
+    log::info!("Handling lambda request");
+    let config = aws_config::load_from_env().await;
+    leetcode_fetch(req.companies, req.difficulties);
+    Ok(())
+}
+
+fn cli_fetch() {
     let args: Args = Args::parse();
 
-    let companies = &args.companies[0];
+    let companies = args.companies;
     let difficulties = args.difficulties;
+    leetcode_fetch(companies, difficulties)
+}
 
+fn leetcode_fetch(companies: Vec<String>, difficulties: Vec<String>) {
+    let mut rng = rand::thread_rng();
     let variables = get_company_tag::Variables {
-        slug: companies.to_string(),
+        slug: companies[rng.gen_range(0..companies.len())].to_string(),
     };
 
-    let cookie = match env::var(COOKIE_ENV_KEY) {
-        Ok(cookie_value) => format!("LEETCODE_SESSION={:#?}", cookie_value),
-        Err(_) => panic!("{} env variable not found", COOKIE_ENV_KEY),
-    };
+    let leetcode_session =
+        env::var(COOKIE_ENV_KEY).expect(&*format!("{} env variable not found", COOKIE_ENV_KEY));
+    let cookie = format!("LEETCODE_SESSION={:#?}", leetcode_session);
     let mut headers = header::HeaderMap::new();
     headers.insert("Cookie", header::HeaderValue::from_str(&*cookie).unwrap());
 
     let leetcode_client = Client::builder()
         .user_agent(USER_AGENT)
         .default_headers(headers)
-        .build()?;
+        .build()
+        .expect("Unable to create leetcode client");
 
     let response_body = post_graphql::<getCompanyTag, _>(
         &leetcode_client,
@@ -53,23 +90,37 @@ fn main() -> Result<(), anyhow::Error> {
         }
     }
 
-    let random_problem_url = LEETCODE_PROBLEMS_PREFIX.to_string() + &result[0];
+    let random_problem_url =
+        LEETCODE_PROBLEMS_PREFIX.to_string() + &result[rng.gen_range(0..result.len())];
 
     let discord_webhook_url = env::var(DISCORD_WEBHOOK_URL_KEY);
     if discord_webhook_url.is_ok() {
         let discord_client = reqwest::blocking::Client::new();
         let body: Value =
-            serde_json::from_str(&*format!("{{\"content\":\"{}\"}}", random_problem_url))?;
+            serde_json::from_str(&*format!("{{\"content\":\"{}\"}}", random_problem_url))
+                .expect("Error creating json from provided url string");
         let response = discord_client
             .post(discord_webhook_url.unwrap())
             .json(&body)
-            .send()?;
+            .send();
+        match response.unwrap().status() {
+            reqwest::StatusCode::OK | reqwest::StatusCode::NO_CONTENT => {
+                log::info!(
+                    "Successfully pushed problem: {} to discord",
+                    random_problem_url
+                );
+            }
+            other => {
+                panic!(
+                    "Error occurred while pushing problem to discord: {:?}",
+                    other
+                );
+            }
+        }
     } else {
-        println!("Discord webhook url is not configured");
-        println!("{}", random_problem_url);
+        log::info!("Discord webhook url is not configured");
+        log::info!("{}", random_problem_url);
     }
-
-    Ok(())
 }
 
 #[derive(GraphQLQuery)]
@@ -81,14 +132,14 @@ fn main() -> Result<(), anyhow::Error> {
 pub struct getCompanyTag;
 
 // Get arguments from user
-#[derive(Parser)]
+#[derive(Parser, Deserialize)]
 #[clap(author, version, about, long_about = None)]
 pub(crate) struct Args {
     // Companies to choose problem from
-    #[clap(short, long)]
+    #[clap(short, long, multiple_values = true)]
     companies: Vec<String>,
 
     // Difficulties to choose problem from
-    #[clap(short, long)]
+    #[clap(short, long, multiple_values = true)]
     difficulties: Vec<String>,
 }
